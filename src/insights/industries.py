1eefeec8d4412b6edaaa6704fa5ad33c22f61d71
washington gama <wgamaneto16@gmail.com>
2023-04-30 11:43:28 -0300

    quase la

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,37 +1,20 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
+    data = read(path)
+    industries = []
+    for industry in data:
+        if industry["industry"] not in industries:
+            if industry["industry"] != "":
+                industries.append(industry["industry"])
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    return industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
+    jobs = [job for job in jobs if job["industry"] == industry]
 
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    return jobs
--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,55 +1,26 @@
+import csv
 from functools import lru_cache
 from typing import List, Dict
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+    with open(path, encoding="utf-8") as file:
+        results = csv.DictReader(file)
+        data = [result for result in results]
+        return data
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    data = read(path)
+    type_jobs = []
+    for job in data:
+        if job["job_type"] not in type_jobs:
+            type_jobs.append(job["job_type"])
+    return sorted(type_jobs)
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
+    jobs_type = [job for job in jobs if job["job_type"] == job_type]
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+    return jobs_type
